69d26958ee982c310add61ec4494ecf0210a236a
Diego Campos <10diegocabj@gmail.com>
2022-03-31 19:21:07 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  
+  public reactiveForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.reactiveForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      age: ['', [Validators.required, Validators.max(180)]],
+      company: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  public onSubmit() {
+    console.table(this.reactiveForm.value);
+  }
 }
